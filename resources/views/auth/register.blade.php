6f718ba60939b838051b00bc1ffaf18632cf3e88
huuthangmst <thang.vh.60cntt@ntu.edu.vn>
2022-11-04 16:56:52 +0700

    add avatar column in database

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,6 +45,20 @@
                                 @enderror
                             </div>
                         </div>
+                        {{-- ảnh --}}
+                        <div class="row mb-3">
+                            <label for="feature_image_path" class="col-md-4 col-form-label text-md-end">{{ __('Avatar') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="feature_image_path" type="file" class="form-control @error('email') is-invalid @enderror" name="feature_image_path" required>
+
+                                @error('feature_image_path')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
