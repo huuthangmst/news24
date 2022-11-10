aef976d8750afd5a022d95fb35d74b80013fee34
huuthang28cb <thang.vh.60cntt@ntu.edu.vn>
2022-11-10 22:14:45 +0700

    update

--- Modules/News/Resources/views/index.blade.php
+++ Modules/News/Resources/views/index.blade.php
@@ -31,7 +31,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="trending-tittle">
-                            <a href="/"><strong>Trending now</strong></a>
+                            <a></a><strong>Trending now</strong>
                             <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
                             <div class="trending-animated">
                                 <ul id="js-news" class="js-hidden">
